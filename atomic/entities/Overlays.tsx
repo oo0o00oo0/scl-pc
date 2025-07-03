1391a5dd0be64c83cec2791524e81bd29eaaf3bf
oo0o00oo0 <dsgnrm@gmail.com>
2025-07-03 15:46:25 +0100

    t

--- atomic/entities/Overlays.tsx
+++ atomic/entities/Overlays.tsx
@@ -1,167 +1,167 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-  Render,
-  Script as ScriptComponent,
-} from "@playcanvas/react/components";
-import { BLEND_NORMAL, Color, Script } from "playcanvas";
-import { Entity } from "@playcanvas/react";
-import { type RenderComponent } from "playcanvas";
-import { useAppStore } from "@/state/appStore";
-import { useModel } from "@playcanvas/react/hooks";
-import { useFilteredUnits } from "@/hooks/useUnits";
-import { useFiltersStore } from "@/state/filtersStore";
-import { useLocation } from "@tanstack/react-router";
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import {
+//   Render,
+//   Script as ScriptComponent,
+// } from "@playcanvas/react/components";
+// import { BLEND_NORMAL, Color, Script } from "playcanvas";
+// import { Entity } from "@playcanvas/react";
+// import { type RenderComponent } from "playcanvas";
+// import { useAppStore } from "@/state/appStore";
+// import { useModel } from "@playcanvas/react/hooks";
+// import { useFilteredUnits } from "@/hooks/useUnits";
+// import { useFiltersStore } from "@/state/filtersStore";
+// import { useLocation } from "@tanstack/react-router";
 
-// @ts-ignore
+// // @ts-ignore
 
-const backupData = [
-  { Plot: "123", Availability: "AVAILABLE" },
-  { Plot: "456", Availability: "UNAVAILABLE" },
-];
+// const backupData = [
+//   { Plot: "123", Availability: "AVAILABLE" },
+//   { Plot: "456", Availability: "UNAVAILABLE" },
+// ];
 
-declare module "playcanvas" {
-  interface Material {
-    emissive: Color;
-    blendType: number;
-    emissiveIntensity: number;
-    opacity: number;
-    update(): void;
-  }
-}
+// declare module "playcanvas" {
+//   interface Material {
+//     emissive: Color;
+//     blendType: number;
+//     emissiveIntensity: number;
+//     opacity: number;
+//     update(): void;
+//   }
+// }
 
-const Overlays = () => {
-  const { pathname } = useLocation();
-  const { asset: model } = useModel("/models/overlays.glb");
-  const { filters } = useFiltersStore();
-  const { setStoreState } = useAppStore();
-  const { data: response } = useFilteredUnits(
-    filters,
-  );
+// const Overlays = () => {
+//   const { pathname } = useLocation();
+//   const { asset: model } = useModel("/models/overlays.glb");
+//   const { filters } = useFiltersStore();
+//   const { setStoreState } = useAppStore();
+//   const { data: response } = useFilteredUnits(
+//     filters,
+//   );
 
-  const residencePopup = useAppStore((state: any) => state.residencePopup);
-  const { data } = response;
-  const selectedUnit = useAppStore((state: any) => state.selectedUnit);
+//   const residencePopup = useAppStore((state: any) => state.residencePopup);
+//   const { data } = response;
+//   const selectedUnit = useAppStore((state: any) => state.selectedUnit);
 
-  const handleModelClick = (unitName: any) => {
-    if (pathname === "/residences" && !residencePopup.isOpen) {
-      const unit = data.find((unit: any) =>
-        unit.unit.replace(" ", "") === unitName
-      );
-      setStoreState({ selectedUnit: unit });
-    }
-  };
+//   const handleModelClick = (unitName: any) => {
+//     if (pathname === "/residences" && !residencePopup.isOpen) {
+//       const unit = data.find((unit: any) =>
+//         unit.unit.replace(" ", "") === unitName
+//       );
+//       setStoreState({ selectedUnit: unit });
+//     }
+//   };
 
-  if (data.length === 0) return null;
+//   if (data.length === 0) return null;
 
-  return (
-    <Entity scale={[-1, 1, -1]}>
-      <Render asset={model as any} type={"asset"}>
-        <ScriptComponent
-          availableIDS={data.map((unit: any) => unit.unit.replace(" ", ""))}
-          script={TestScript}
-          callback={handleModelClick}
-          activePlot={selectedUnit?.unit.replace(" ", "")}
-          disable={residencePopup.isOpen}
-        />
-      </Render>
-    </Entity>
-  );
-};
+//   return (
+//     <Entity scale={[-1, 1, -1]}>
+//       <Render asset={model as any} type={"asset"}>
+//         <ScriptComponent
+//           availableIDS={data.map((unit: any) => unit.unit.replace(" ", ""))}
+//           script={TestScript}
+//           callback={handleModelClick}
+//           activePlot={selectedUnit?.unit.replace(" ", "")}
+//           disable={residencePopup.isOpen}
+//         />
+//       </Render>
+//     </Entity>
+//   );
+// };
 
-class TestScript extends Script {
-  callback: (data: any) => void = () => {};
+// class TestScript extends Script {
+//   callback: (data: any) => void = () => {};
 
-  // private readonly selectedColor = new Color(0.90, 0.91, 0.92);
-  private readonly selectedColor = new Color(0.24, 0.30, 0.30);
-  // private readonly defaultColor = new Color(0, 0, 0);
-  private _models: any[] | null = null;
-  private _activePlot: string | null = null;
-  private _availableIDS: string[] = [];
-  private _disable: boolean = false;
-  set availableIDS(v: string[]) {
-    this._availableIDS = v;
-  }
-  get availableIDS() {
-    return this._availableIDS;
-  }
+//   // private readonly selectedColor = new Color(0.90, 0.91, 0.92);
+//   private readonly selectedColor = new Color(0.24, 0.30, 0.30);
+//   // private readonly defaultColor = new Color(0, 0, 0);
+//   private _models: any[] | null = null;
+//   private _activePlot: string | null = null;
+//   private _availableIDS: string[] = [];
+//   private _disable: boolean = false;
+//   set availableIDS(v: string[]) {
+//     this._availableIDS = v;
+//   }
+//   get availableIDS() {
+//     return this._availableIDS;
+//   }
 
-  set disable(v: boolean) {
-    this._disable = v;
-    if (this._models) this.updateModels(this._models);
-  }
-  get disable() {
-    return this._disable;
-  }
+//   set disable(v: boolean) {
+//     this._disable = v;
+//     if (this._models) this.updateModels(this._models);
+//   }
+//   get disable() {
+//     return this._disable;
+//   }
 
-  set activePlot(v: string | null) {
-    if (this._activePlot !== v) {
-      this._activePlot = v;
+//   set activePlot(v: string | null) {
+//     if (this._activePlot !== v) {
+//       this._activePlot = v;
 
-      if (this._models) this.updateModels(this._models);
-    }
-  }
-  get activePlot() {
-    return this._activePlot;
-  }
+//       if (this._models) this.updateModels(this._models);
+//     }
+//   }
+//   get activePlot() {
+//     return this._activePlot;
+//   }
 
-  updateModels(models: any[]) {
-    models.forEach((model) => {
-      const isSelected = this.activePlot === model.name;
+//   updateModels(models: any[]) {
+//     models.forEach((model) => {
+//       const isSelected = this.activePlot === model.name;
 
-      const render = model.render as RenderComponent;
+//       const render = model.render as RenderComponent;
 
-      model.enabled = !this._disable;
+//       model.enabled = !this._disable;
 
-      if (render?.meshInstances) {
-        let needsUpdate = false;
+//       if (render?.meshInstances) {
+//         let needsUpdate = false;
 
-        render.meshInstances.forEach((mi) => {
-          if (mi.material.opacity !== (isSelected ? 0.7 : 0.0)) {
-            mi.material.opacity = isSelected ? 0.7 : 0.0;
-            needsUpdate = true;
-          }
-        });
+//         render.meshInstances.forEach((mi) => {
+//           if (mi.material.opacity !== (isSelected ? 0.7 : 0.0)) {
+//             mi.material.opacity = isSelected ? 0.7 : 0.0;
+//             needsUpdate = true;
+//           }
+//         });
 
-        if (needsUpdate) {
-          render.meshInstances.forEach((mi) => mi.material.update());
-          this.app.renderNextFrame = true;
-        }
-      }
-    });
-  }
+//         if (needsUpdate) {
+//           render.meshInstances.forEach((mi) => mi.material.update());
+//           this.app.renderNextFrame = true;
+//         }
+//       }
+//     });
+//   }
 
-  initialize() {
-    const immediateLayer = this.app.scene.layers.getLayerByName("Immediate");
-    this._models = this.entity.children[0].children;
+//   initialize() {
+//     const immediateLayer = this.app.scene.layers.getLayerByName("Immediate");
+//     this._models = this.entity.children[0].children;
 
-    this._models.forEach((model) => {
-      model.on("click", () => {
-        const name = model.name;
+//     this._models.forEach((model) => {
+//       model.on("click", () => {
+//         const name = model.name;
 
-        if (this.callback) this.callback(name);
+//         if (this.callback) this.callback(name);
 
-        // this.updateModels(this._models!);
-      });
+//         // this.updateModels(this._models!);
+//       });
 
-      const render = model.render as RenderComponent;
+//       const render = model.render as RenderComponent;
 
-      if (immediateLayer && render?.meshInstances) {
-        render.layers = [immediateLayer.id];
-        render.meshInstances.forEach((mi) => {
-          // mi.material.emissive.copy(
-          //   enabled ? this.defaultColor : this.unavailableColor,
-          // );
-          mi.material.emissive.copy(this.selectedColor);
+//       if (immediateLayer && render?.meshInstances) {
+//         render.layers = [immediateLayer.id];
+//         render.meshInstances.forEach((mi) => {
+//           // mi.material.emissive.copy(
+//           //   enabled ? this.defaultColor : this.unavailableColor,
+//           // );
+//           mi.material.emissive.copy(this.selectedColor);
 
-          mi.material.opacity = 0.0;
-          mi.material.blendType = BLEND_NORMAL;
-          mi.material.update();
-        });
-      }
-    });
+//           mi.material.opacity = 0.0;
+//           mi.material.blendType = BLEND_NORMAL;
+//           mi.material.update();
+//         });
+//       }
+//     });
 
-    if (this._activePlot) this.updateModels(this._models);
-  }
-}
+//     if (this._activePlot) this.updateModels(this._models);
+//   }
+// }
 
-export default Overlays;
+// export default Overlays;
